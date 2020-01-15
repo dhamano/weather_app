2bebfec1201f5c6f4afaa92ddac14c3cd27e5bcb
Dustin <clerin+gitkraken@gmail.com>
2020-01-14 21:36:27 -0800

    added footer with link to api and copyright info.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 // import Navigation from './components/Navigation';
 import DisplayWeather from './components/DisplayWeather';
 import Search from './components/Search';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Search />
       <DisplayWeather />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="copyright">
+                &copy; 2019&#8202;&#8211;&#8202;{ new Date().getFullYear() } Dustin Hamano. All Rights Reserved.
+            </p>
+            <p>
+                Weather pulled from <a href="https://www.metaweather.com/" target="_blank" rel="noopener noreferrer">www.metaweather.com</a>.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
