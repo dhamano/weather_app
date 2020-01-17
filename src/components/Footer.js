e2c08513388de8f8d39965cd4f739d997f50695d
Dustin <clerin+gitkraken@gmail.com>
2020-01-16 16:48:11 -0800

    updated footer to use getUTCFullYear instead of getFullYear to keep consistent with getUTCDay instead of getDay.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <p className="copyright">
-                &copy; 2019&#8202;&#8211;&#8202;{ new Date().getFullYear() } Dustin Hamano. All Rights Reserved.
+                &copy; 2019&#8202;&#8211;&#8202;{ new Date().getUTCFullYear() } Dustin Hamano. All Rights Reserved.
             </p>
             <p>
                 Weather pulled from <a href="https://www.metaweather.com/" target="_blank" rel="noopener noreferrer">www.metaweather.com</a>.
